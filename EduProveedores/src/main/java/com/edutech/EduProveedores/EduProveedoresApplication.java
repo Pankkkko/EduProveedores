46e7366745cfe3f1b819fea2af34fba560543227
Tomas <to.bustosm@duocuc.cl>
2025-06-16 18:50:30 -0400

    first commit

---
+++ EduProveedores/src/main/java/com/edutech/EduProveedores/EduProveedoresApplication.java
@@ -0,0 +1,13 @@
+package com.edutech.EduProveedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduProveedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduProveedoresApplication.class, args);
+	}
+
+}
---
+++ EduProveedores/src/test/java/com/edutech/EduProveedores/EduProveedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutech.EduProveedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduProveedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
